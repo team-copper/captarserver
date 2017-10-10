751ef6e346ec324f8fabc0e8468301f065dc7364
Oscar A. Ramirez <oscar.arturo.ramirez@gmail.com>
2017-10-10 13:47:22 -0400

    should never be in repo

--- secrets.js
+++
@@ -1,9 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '559034224899-anhvq493d65jlk2ln7sapopnl5btmno2.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'oUWBPBDKycDbAfVYmAuGg9Zd'
-process.env.GOOGLE_CALLBACK = 'http://localhost:8080/auth/google/callback'
-process.env.SESSION_SECRET = 'sup3rcal1fragilistic3xpialidoc1ous'
-
-process.env.FACEBOOK_CLIENT_ID = '131105214208608'
-process.env.FACEBOOK_CLIENT_SECRET = '963af59430f9ac58fdb60d10e3567030'
-process.env.FACEBOOK_CALLBACK = 'http://localhost:8080/auth/facebook/callback'
-process.env.SESSION_SECRET = 'sup3rcal1fragilistic3xpialidoc1ous'
